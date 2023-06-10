7b98379d4f7eacfe0bab38e09da8946c3a2eb7e7
Althuaf <althuafalthaf@gmail.com>
2023-06-10 12:24:34 +0530

    Added new categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css';
-import {action,originals} from'./urls'
+import {action,documentaries,originals,romance,horror,comedy} from'./urls'
 import NavBar from './Components/NavBar/NavBar';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
@@ -9,9 +9,13 @@ function App() {
   return (
     <div className="App">
      <NavBar/>
-     <Banner/>
+     <Banner/><br/>
      <RowPost url={originals} title='Netflix Originals'/>
      <RowPost url={action} title='Action' isSmall/>
+     <RowPost url={comedy} title='Comedy' isSmall/>
+     <RowPost url={horror} title='Horror' isSmall/>
+     <RowPost url={romance} title='Romance' isSmall/>
+     <RowPost url={documentaries} title='Dcumentaries' isSmall/>
     </div>
   );
 }
